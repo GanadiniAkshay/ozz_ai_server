c57de694847a5f5042f198e114aa334a0be7ee79
Akshay <akshaykulkarni.2104@gmail.com>
2017-10-08 03:44:53 +0530

    moved CDN to os config to avoid docker builds on UI updates

--- project/config.py
+++ project/config.py
@@ -6,7 +6,7 @@ class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'my_precious'
-    CDN_URL = 'https://d1wi3kcd7kachl.cloudfront.net/v0.4.12'
+    CDN_URL = os.environ.get('CDN_URL')
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
