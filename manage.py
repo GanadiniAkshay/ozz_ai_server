376a69eb96537385a9eba0ef525e37f57d624e9b
Akshay <akshaykulkarni.2104@gmail.com>
2017-10-24 02:28:21 +0530

    removed manage.py dependency

--- manage.py
+++ manage.py
@@ -13,9 +13,6 @@ from project.api.models.schedule import Schedule
 from project.api.models.logs import Logs
 from project.api.models.knowledge import Knowledge
 
-from flask_migrate import MigrateCommand
-from flask_pymongo import PyMongo
-
 app = create_app()
 manager = Manager(app)
 user_tsv_file = 'users.tsv'
