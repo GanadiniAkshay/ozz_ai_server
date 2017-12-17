dfa8b310295963fa8da014817232a6f3af0a997e
Akshay <akshaykulkarni.2104@gmail.com>
2017-12-17 07:34:50 +0530

    changed html

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 export DATABASE_URL=postgresql://localhost/ozz_dev
 export APP_SETTINGS=project.config.DevelopmentConfig
-export CDN_URL=https://d1wi3kcd7kachl.cloudfront.net/v0.8.5
+export CDN_URL=https://d1wi3kcd7kachl.cloudfront.net/v0.9.0
 export REDIS_HOST=localhost
 export REDIS_PORT=6379
 
